fc30e32e91b6020d97061ba3fbd1b9ba86d09bbb
Deivid Costa <deivid652015@gmail.com>
2025-07-22 00:54:52 -0300

    refatorar estrutura do projeto: remover arquivos não utilizados e adicionar componente Nome

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Nome from './components/Nome'
+function App(){
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return(
+
+  <>
+  <div>
+    <h1>Componente App</h1>
+    <Nome aluno ="Lucas"/>
+    <br/>
+    <Nome aluno ="Jose Lucas"/>
+  </div>
+  </>    
   );
+
 }
 
 export default App;
---
+++ src/components/Nome.js
@@ -0,0 +1,8 @@
+function Nome({aluno}){
+
+  return(
+    <span>Bem vindo {aluno}</span>
+
+  );
+}
+export default Nome;
